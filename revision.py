3d8bb1851129cbb484251b8f06ffd4cf52299b33
benarzee-ben1212 <90883894+benarzee-ben1212@users.noreply.github.com>
2024-03-28 15:00:42 +0530

    revision class

---
+++ revision.py
@@ -0,0 +1,11 @@
+# Example 7: count()
+s = 'hello world'
+print(s.count('l'))  # Output: 3
+
+# Example 8: replace()
+s = 'hello world'
+print(s.replace('world', 'python'))  # Output: hello python
+
+# Example 9: split()
+s = 'hello world'
+print(s.split())  # Output: ['hello', 'world']
